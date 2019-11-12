6a4e448927500bc30aa41f53333d3813adaa5c50
Wilply <perso@clfp.fr>
2019-11-12 23:43:24 +0100

    update lemp 9

--- 05_lemp/app/index.php
+++ 05_lemp/app/index.php
@@ -4,5 +4,16 @@
  </head>
  <body>
   <?php echo '<p>SI CA S AFFICHE ALORS PHP FONCTIONNE</p>'; ?>
+  <p>SI CA S AFFICHE ALORS PHP FONCTIONNE</p>
+  bite
+  <br>
+  <br>
+  grosse
+  <br>
+  <br>
+  a sucer
+  <br>
+  <br>
+  
  </body>
 </html>
